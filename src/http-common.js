10cc61c163d8d11522efa83849cdc8d1a79756fc
Ced-Mich Pro-Tail <37204120+ced210@users.noreply.github.com>
2020-10-03 01:17:24 -0400

    lil fix

--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL:
     process.env.NODE_ENV !== "production"
       ? "http://localhost:8081/api"
-      : "https://characterbook-api.azurewebsites.net/api/",
+      : "https://characterbook-api.azurewebsites.net/api",
 
   headers: {
     "Content-type": "application/json",
