4269db4482272e67202fc3dccbcd3fab851a1575
Ced-Mich Pro-Tail <37204120+ced210@users.noreply.github.com>
2020-10-03 16:51:27 -0400

    fix http-common for dev and prod

--- src/http-common.js
+++ src/http-common.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    // process.env.NODE_ENV !== "production"
-    // ? "http://localhost:8081/api"
-    // :
-    "https://characterbook-api.azurewebsites.net/api",
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:8081/api"
+      : "https://characterbook-api.azurewebsites.net/api",
 
   headers: {
     "Content-type": "application/json",
