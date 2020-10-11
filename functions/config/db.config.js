6c7499a6bf61e9c174ecae2139557d502113eb96
Ced-Mich Pro-Tail <37204120+ced210@users.noreply.github.com>
2020-10-10 21:59:30 -0400

    set up config for new DB host in prod

--- functions/config/db.config.js
+++ functions/config/db.config.js
@@ -3,11 +3,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "developpement";
 let HOST = "localhost";
 let USER = "root";
 let PASSWORD = "!Book!17";
+let DB = "characterbook";
 if (process.env.NODE_ENV === "test") {
 } else if (process.env.NODE_ENV === "production") {
-  HOST = "characterbook.mysql.database.azure.com";
-  USER = "admin210@characterbook";
-  PASSWORD = "!Book!17";
+  HOST = "92.249.45.155";
+  USER = "u727565515_anubis210";
+  DB = "u727565515_characterbook";
 } else if (process.env.NODE_ENV === "developpement") {
   HOST = "localhost";
   USER = "root";
@@ -16,7 +17,7 @@ module.exports = {
   HOST: HOST,
   USER: USER,
   PASSWORD: PASSWORD,
-  DB: "characterbook",
+  DB: DB,
   dialect: "mysql",
   port: 3306,
   pool: {
